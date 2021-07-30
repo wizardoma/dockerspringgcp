a4bb07a7e791b462fb65bde1c275927ba7c47066
Ibekason Alexander <alibekason@gmail.com>
2021-07-30 11:23:24 +0100

    Add journal server microservice

---
+++ journal_server/src/main/java/com/wizardom/journal_server/JournalServerApplication.java
@@ -0,0 +1,13 @@
+package com.wizardom.journal_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JournalServerApplication.class, args);
+    }
+
+}
